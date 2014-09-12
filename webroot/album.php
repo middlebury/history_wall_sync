c18150b7b868c18f9ea8c90fae3a408e96a59df0
Adam Franco <afranco@middlebury.edu>
2014-09-12 13:21:52 -0400

    The allowed-category list is now displayed on reports.

--- webroot/album.php
+++ webroot/album.php
@@ -14,69 +14,19 @@ $photoset = (object)$flickr->photosets_getInfo($photoset_id);
 	<link rel="stylesheet" href="report.css" type="text/css" />
 </head>
 <body>
-	<a href='index.php'>&laquo; Back</a>
+	<?php include(dirname(__FILE__).'/header.php'); ?>
+	
 	<h1>Middlebury Flickr - History Wall Report</h1>
 	<h2>Album: <?php print implode(' ', $photoset->title); ?></h2>
 	
 <?php
-
-$categories = array(
-	'Women’s Basketball',
-	'Men’s Basketball',
-	'Baseball',
-	'Softball',
-	'Women’s Cross Country',
-	'Men’s Cross Country',
-	'Field Hockey',
-	'Football',
-	'Women’s Golf',
-	'Men’s Golf',
-	'Women’s Indoor Track',
-	'Men’s Indoor Track',
-	'Women’s Hockey',
-	'Men’s Hockey',
-	'Women’s Soccer',
-	'Men’s Soccer',
-	'Women’s Skiing',
-	'Men’s Skiing',
-	'Women’s Lacrosse',
-	'Men’s Lacrosse',
-	'Women’s Squash',
-	'Men’s Squash',
-	'Women’s Track and Field',
-	'Men’s Track and Field',
-	'Women’s Tennis',
-	'Men’s Tennis',
-	'Women’s Swimming and Diving',
-	'Men’s Swimming and Diving',
-	'Volleyball',
-	'Champions',
-	'Traditions',
-	'Women in Sports',
-	'Team Photo',
-	'Celebrations',
-	'Recreational Sports',
-	'Facilities',
-	'Headlines ',
-	'Coaches',
-	'Community and Service',
-	'Rivalries',
-	'Olympians and Pros',
-	'Club Sports',
-	'Cheer and Spirit',
-	'Legacy ',
-	'Campus Life',
-	'Intramurals',
-	'Ephemera',
-	'Illustrations',			
-);
 
 $args = array(
 	'photoset_id' => $photoset_id,
 	'extras' => 'description,date_taken,url_o,url_t,tags,machine_tags',
 );
 
-$printer = new PhotoPrinter($categories);
+$printer = new PhotoPrinter($WALL_CATEGORIES);
 $printer->output(
 	new PhotoSetIterator($flickr, $args, $printer->getStartingPhotoOffset(), 100)
 );
---
+++ webroot/header.php
@@ -0,0 +1,7 @@
+	<div id='category_area'>
+		<h4>Allowed Wall Categories</h4>
+		<ul id='categories'>
+			<li><?php print implode("</li>\n\t\t\t<li>", $WALL_CATEGORIES); ?></li>
+		</ul>
+	</div>
+	<a href='index.php'>&laquo; Back</a>
